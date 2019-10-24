bb5684faea582fb7c6492f20547220817891b23e
yasarcu <yasarcu@hotmail.com>
2019-10-24 22:43:20 +0200

    1977 + 1 = leblebi yazsin

--- hesapla.js
+++ hesapla.js
@@ -1,5 +1,10 @@
 function calcValue(value)
 {
+    if((document.Calc.Input.value)=='1977 + 1')
+    {
+        var y = 1837837;
+        return(y);
+    }
 var x = eval(document.Calc.Input.value);
 
 var y = Math.round(x*10000)/10000;
